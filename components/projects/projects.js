d0312401e20cc7228f0ebef8d6db39dcb20fec9a
Richard Hancock <breakerbay@hotmail.com>
2018-01-01 11:21:04 +1300

    Added projects.js

---
+++ components/projects/projects.js
@@ -0,0 +1,8 @@
+/**
+ * Created by richardhancock on 1/01/18.
+ */
+app.controller('ProjectsCtrl', ['$scope', function ($scope) {
+}]);
+
+app.controller('ProjectCtrl', ['$scope', function ($scope) {
+}]);
